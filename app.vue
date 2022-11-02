803546f598aebc19ad8d0aaaa7287fd4d958d3e4
MrGultekin <gultekin.de@gmail.com>
2022-11-02 16:20:05 +0100

    it works

--- app.vue
+++ app.vue
@@ -10,41 +10,22 @@ const numberOfPhotos = computed(() => {
   return photoGallery.value.length;
 })
 
-const evenAlbums = computed(()=> {
+const evenAlbums = computed(() => {
   return photoGallery.value.filter(item => item.albumId % 2 === 0)
 })
-const oddAlbums = computed(()=> {
+const oddAlbums = computed(() => {
   return photoGallery.value.filter(item => item.albumId % 2 === 1)
 })
 
 
-// export default defineNuxtComponent({
-//
-//   data: () => ({
-//     photoGallery: [],
-//   }),
-//   computed: {
-//     numberOfPhotos() {
-//       return this.photoGallery.length;
-//     },
-//     evenAlbums() {
-//        return this.photoGallery.filter(item => item.albumId % 2 === 0)
-//     },
-//     oddAlbums() {
-//        return this.photoGallery.filter(item => !(item.albumId % 2 === 0))
-//     }
-//   },
-//
-//   methods: {
-//     fetchPhotoGallery() {
-//       fetch('https://jsonplaceholder.typicode.com/photos')
-//           .then(response => response.json())
-//           .then(json => {
-//             this.photoGallery = json
-//           })
-//     }
-//   }
-// })
+function fetchPhotoGallery(){
+  fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(json => {
+        photoGallery.value = json
+      })
+}
+
 </script>
 
 <template>
