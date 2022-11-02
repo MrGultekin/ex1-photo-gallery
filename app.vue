9ed229a80dc8c281d908eb7c77661e752e924772
MrGultekin <gultekin.de@gmail.com>
2022-11-02 15:09:11 +0100

    computed funcs work.

--- app.vue
+++ app.vue
@@ -4,8 +4,20 @@ import {defineNuxtComponent} from '#app'
 export default defineNuxtComponent({
 
   data: () => ({
-    photoGallery: []
+    photoGallery: [],
   }),
+  computed: {
+    numberOfPhotos() {
+      return this.photoGallery.length;
+    },
+    evenAlbums() {
+       return this.photoGallery.filter(item => item.albumId % 2 === 0)
+    },
+    oddAlbums() {
+       return this.photoGallery.filter(item => !(item.albumId % 2 === 0))
+    }
+  },
+
   methods: {
     fetchPhotoGallery() {
       fetch('https://jsonplaceholder.typicode.com/photos')
@@ -23,6 +35,9 @@ export default defineNuxtComponent({
     <h1>Photo Gallery</h1>
 
     <button @click="fetchPhotoGallery">Fetch Photos</button>
+    <p> Total: {{ numberOfPhotos }} photos are available. </p>
+    <p> Even: {{ evenAlbums.length }} photos are available. </p>
+    <p> Odd: {{ oddAlbums.length }} photos are available. </p>
     <ul>
       <li v-for="photo in photoGallery" :key=" `photo-id-${photo.id}` ">
         <img :src="photo.thumbnailUrl" alt="colored photos">
