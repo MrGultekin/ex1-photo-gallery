fd5756aa0387546cbe66aa66ca1184720f3d8c23
MrGultekin <gultekin.de@gmail.com>
2022-11-02 14:48:42 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script>
+import {defineNuxtComponent} from '#app'
+
+export default defineNuxtComponent({
+
+  data: () => ({
+    photoGallery: []
+  }),
+  methods: {
+    fetchPhotoGallery() {
+      fetch('https://jsonplaceholder.typicode.com/photos')
+          .then(response => response.json())
+          .then(json => {
+            this.photoGallery = json
+          })
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>Photo Gallery</h1>
+
+    <button @click="fetchPhotoGallery">Fetch Photos</button>
+    <ul>
+      <li v-for="photo in photoGallery" :key=" `photo-id-${photo.id}` ">
+        <img :src="photo.url" alt="colored photos">
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<style></style>
